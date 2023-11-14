5f7029a729f66a13eb7a4bac8ff26eb71ef1d9ad
Edgar Emir Borbón Jimenez <edemboji@gmail.com>
2023-11-13 17:50:53 -0700

    Se arreglaron detalles del jar

--- src/Persistence/src/main/java/com/mycompany/persistence/Persistence.java
+++ src/Persistence/src/main/java/com/mycompany/persistence/Persistence.java
@@ -1,9 +1,10 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  */
-package com.mycompany.persistencia;
 
-import daos.DataBaseConnection;
+package com.mycompany.persistence;
+
+import db.DataBaseConnection;
 import domain.User;
 import javax.persistence.EntityManager;
 
@@ -11,12 +12,11 @@ import javax.persistence.EntityManager;
  *
  * @author edemb
  */
-public class Main {
+public class Persistence {
 
     public static void main(String[] args) {
-        EntityManager entityManager = DataBaseConnection.createConnection("website");
+        EntityManager entityManager = DataBaseConnection.createConnection("webProject");
         entityManager.getTransaction().begin();
         entityManager.persist(new User("Emir"));
-        entityManager.getTransaction().commit();
-    }
+        entityManager.getTransaction().commit();    }
 }
