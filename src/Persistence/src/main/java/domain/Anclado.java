1cff44a6f5acc0b60151f1f25458e165de85b8ab
JIVB <darksamus0997@hotmail.com>
2023-11-13 18:35:37 -0700

    feat: Anclado creado

---
+++ src/Persistence/src/main/java/domain/Anclado.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package domain;
+
+import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+/**
+ *
+ * @author JIVB
+ */
+@Entity
+public class Anclado extends Post implements Serializable {
+    
+}
