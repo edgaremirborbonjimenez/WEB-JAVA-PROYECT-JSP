8ce3e7581884626428ba4f647141ae6ddeb545b8
JIVB <darksamus0997@hotmail.com>
2023-11-13 18:49:44 -0700

    feat: Normal creado

---
+++ src/Persistence/src/main/java/domain/Normal.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package domain;
+
+import java.io.Serializable;
+import javax.persistence.Entity;
+
+/**
+ *
+ * @author JIVB
+ */
+@Entity
+public class Normal extends Usuario implements Serializable {
+    
+}
