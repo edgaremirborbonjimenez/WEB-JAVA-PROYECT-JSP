a9648a2b6c58da87a23342292747d8cfd13bf39c
JIVB <darksamus0997@hotmail.com>
2023-11-13 18:48:45 -0700

    feat: Admor cambios ligeros

--- src/Persistence/src/main/java/domain/Admor.java
+++ src/Persistence/src/main/java/domain/Admor.java
@@ -6,9 +6,6 @@ package domain;
 
 import java.io.Serializable;
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
 
 /**
  *
