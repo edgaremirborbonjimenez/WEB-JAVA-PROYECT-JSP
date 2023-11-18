d4bd700b5742c6ba4c9bff7a9b740280b0852585
Chinchillas <79560217+chinchillas03@users.noreply.github.com>
2023-11-18 16:43:13 -0700

    Update Testing.java
    Se realizo el test del caso de crear post comun

--- src/Persistence/src/test/java/testing/Testing.java
+++ src/Persistence/src/test/java/testing/Testing.java
@@ -5,8 +5,10 @@ package testing;
  * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit5TestClass.java to edit this template
  */
 import dao.interfaces.Persistencia;
+import domain.Comun;
 import domain.Estado;
 import domain.Municipio;
+import domain.Post;
 import domain.Usuario;
 import fachada.FachadaPersistencia;
 import java.util.Date;
@@ -40,4 +42,18 @@ public class Testing {
         assertNotNull(usuer);
     }
 
+    @Test
+    public void crearPublicacionTest(){
+        Post post = null;
+        Usuario user = null;
+        try {
+            Municipio municipios = fachadaPersistenia.registrarMunicipio(new Municipio("Puebla", new Estado("Nacho")));
+            user = fachadaPersistenia.registrarUsuario(new Usuario("Antonio", "antonio@gmail.com", "1234", "1234556778", "Perro", "Navojoa", new Date(), "Hombre",municipios));
+
+            post = fachadaPersistenia.crearPost(new Comun(user, null, new Date(2023, 11, 18, 16, 18, 33), null, "Prueba post", "Los quiero como test"));
+        } catch (Exception e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+    }
+    
 }
