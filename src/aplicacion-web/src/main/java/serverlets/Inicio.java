696cab1fe42e79c469ab61b8f6b7f85d06acd17d
JIVB <darksamus0997@hotmail.com>
2023-11-29 23:54:18 -0700

    feat: Cambios para ver publicaciones incompleto

---
+++ src/aplicacion-web/src/main/java/serverlets/Inicio.java
@@ -0,0 +1,107 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
+ */
+package serverlets;
+
+import dao.interfaces.Persistencia;
+import domain.Post;
+import fachada.FachadaPersistencia;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Date;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author JIVB
+ */
+@WebServlet(name = "Inicio", urlPatterns = {"/Inicio"})
+public class Inicio extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet Inicio</title>");            
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>Servlet Inicio at " + request.getContextPath() + "</h1>");
+            out.println("</body>");
+            out.println("</html>");
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        //processRequest(request, response);
+        Persistencia p = new FachadaPersistencia();
+        var post = new Post(new Date(), new Date(), "Publicacion de prueba", "en esta publicacion solo sera texto xd");
+        var post2 = new Post(new Date(), new Date(), "Publicacion de numero 2", "ojala tubiera ideas de mas publicaciones para que sea mas largo");
+        var post3 = new Post(new Date(), new Date(), "Publicacion de prueba 3", "soy relleno para hacer mas publicaciones jje");
+        try {
+            p.crearPost(post);
+            p.crearPost(post2);
+            p.crearPost(post2);
+        } catch (Exception ex) {
+            Logger.getLogger(Inicio.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        response.setContentType("text/plain");
+        response.setCharacterEncoding("UTF-8");
+        response.getWriter().write("mis frijolesss!!");
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
---
+++ src/aplicacion-web/src/main/webapp/inicio.jsp
@@ -0,0 +1,43 @@
+<%-- 
+    Document   : Inicio
+    Created on : 28 nov 2023, 10:11:38
+    Author     : JIVB
+--%>
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <link rel='stylesheet' type='text/css' media='screen' href='style.css'>
+        <link rel='stylesheet' type='text/css' media='screen' href='Publicacion.css'>
+        <title>Feizbuk Inicio</title>
+    </head>
+    <body>
+        <%@include file="WEB-INF/Header.jspf" %>
+        <main>
+		<button class="p">¡Vamos pública algo, haz clic aqui!</button>
+
+		<form action="">
+			<div class="f">
+                            <h3 id="autor">De: Roberto</h3>
+				<img class="basura" src="https://cdn3.iconfinder.com/data/icons/user-interface-169/32/trash-512.png">
+			</div>
+                    <h5 id="titulo">mi primera publicacion</h5>
+                        <p id="texto"></p>
+			<p>comentarios</p>
+			<div class="coms">
+				<p>Luis:</p>
+				<p>Marco:</p>
+				<p>Pedro:</p>
+				<p>Luisa:</p>
+			</div>
+			<div class="in">
+				<input type="text" id="comentario" placeholder="Escribele un comentario:">
+				<button type="button">Envíar</button>
+			</div>
+		</form>
+	</main>
+                        <script src="inicio.js"></script>
+    </body>
+</html>
